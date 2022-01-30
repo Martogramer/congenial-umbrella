1495e466b70b8efc444b28bb7844ab85d6dcfe91
Marto <contact@torox.com>
2022-01-30 10:54:04 -0300

    first commit

---
+++ api/src/models/Genres.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('genres', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+};
