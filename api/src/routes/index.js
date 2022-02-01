17c3c66dadd29fd116a6ab606d37d2791ac82bc5
Marto <contact@torox.com>
2022-01-31 21:15:31 -0300

    ruta principal

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,9 @@
 const { Router } = require("express");
-const { express } = require("express");
-
 const gamesRoute = require('./games');
 
 const router = Router();
 
 router.use("/games", gamesRoute);
-
+router.use("/games?name=", gamesRoute)
 
 module.exports = router;
