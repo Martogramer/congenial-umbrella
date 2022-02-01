e4e43ac3e354fbfef7643a04a8a1525c90cbb08e
Marto <contact@torox.com>
2022-01-31 21:13:28 -0300

    cambios en ruta principal

--- api/src/routes/games.js
+++ api/src/routes/games.js
@@ -1,16 +1,27 @@
-const { getAllInfo } = require('../ctrl/get.controllers');
-
+const { getAllInfo } = require("../ctrl/get.controllers");
 const { Router } = require("express");
+
 const router = Router();
 
-router.get('/', async (req, res) => {
-    const name = req.query.name
+const getByName = async (req, res, next) => {
+  const name = req.query.name;
+  const allGames = await getAllInfo();
+
+  try {
     if (!name) {
-        return res.status(200).send(await getAllInfo());
-    }else {
-        const game = getAllInfo.filter(e=>e.name.toLowerCase().include(name));
-        return res.status(200).send(game);
+      return res.status(200).send(allGames);
+    } else {
+      const game = allGames.filter((e) =>
+        e.name.toLowerCase().includes(name.toLowerCase())
+      );
+      game.lenght
+        ? res.status(200).json(game)
+        : res.status(404).send("No Se Encontraron Resultados");
     }
-})
+  } catch (err) {
+    return error;
+  }
+};
+router.get("/", getByName);
 
 module.exports = router;
