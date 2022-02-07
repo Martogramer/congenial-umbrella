c8e354ea3542cf5da8c125f783d35b6c006b40ee
Marto <contact@torox.com>
2022-02-07 11:28:55 -0300

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import React, { Fragment } from 'react';
+import Home from './components/home/Home';
+import Landing from './components/landing/Landing';
 
-function App() {
+
+ function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
+    <Fragment>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  </Fragment>
   );
 }
 
