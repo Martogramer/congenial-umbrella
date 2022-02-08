6fc59fd742dce390dd3974a5366d1eefb215eaec
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-02-08 00:46:01 -0300

    home

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,29 +2,32 @@ import React from 'react';
 import {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import { getGames } from '../../actions';
+import {Link} from 'react-router-dom';
 
 
-export default function Home (){
+const Home=()=>{
     const dispatch= useDispatch()
     const games = useSelector((state)=>state.games)
 
     useEffect (()=>{
         dispatch(getGames())
-    }, [])
+    })
 
     function handleClick(e) {
         e.preventDefault();
         dispatch(getGames())
     }
 
-
+    
     return (
         <div>
-            <button onClick={handleClick()}>
-                Refresh
+            <Link to='/'><a>volver</a></Link>
+            <button onClick={e=> {handleClick(e)}}>
+                sarasa
             </button>
+            <h1>PARATUSALSA</h1>
         </div>
     )
 
 }
-
+export default Home;
