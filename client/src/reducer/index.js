96b8a9474bc44f84b444f52bfe5fd9131094fcc1
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-02-09 09:37:57 -0300

    cambios en reducer

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,14 +1,25 @@
+import { GET_GAMES, LOAD_GAMES } from "../actions";
+
 const initialState = {
-    games: [],
+    videogames: [],
+    loading: false
 }
 
 function rootReducer (state = initialState, action) {
+    // eslint-disable-next-line default-case
     switch (action.type) {
-        case 'GET_GAMES':
+        case GET_GAMES:
             return {
                 ...state,
-                games: action.payload,
+                videogames: action.payload,
+                allGames: action.payload
             }
+        case LOAD_GAMES:
+            return {
+                ...state,
+                loading: action.payload
+            }
+
     }
 }
 export default rootReducer;
