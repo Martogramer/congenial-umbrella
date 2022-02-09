9580dd642517f02a5149636a2591e01033bee435
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-02-09 09:37:28 -0300

    componente principal videogames

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,29 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-<<<<<<< HEAD
 import {Link} from 'react-router-dom'
-=======
-import { getGames } from '../../actions';
-import {Link} from 'react-router-dom';
->>>>>>> 5bb1eac219e4adf986593038384435e04e67fe19
 
 import { getGames, loadingLoad } from '../../actions';
+import Filter from '../filter/Filter';
+import Card from '../card/Card';
+import Pagination from '../pagination/Pagination';
 
-<<<<<<< HEAD
-
-Videogames=()=>{
+const Videogames=()=>{
     let games=useSelector((state)=>state.games)
-    let loading=useSelector((state)=>state.loading)
-    const [videogames, setVideogames]=useState([]);
-=======
-const Home=()=>{
-    const dispatch= useDispatch()
-    const games = useSelector((state)=>state.games)
-
-    useEffect (()=>{
-        dispatch(getGames())
-    })
->>>>>>> 5bb1eac219e4adf986593038384435e04e67fe19
+    //let loading=useSelector((state)=>state.loading)
+    //const [videogames, setVideogames]=useState([]);
 
     const [order, setOrder] = useState('')
     const [currentPage, setCurrentPage] = useState(1)
@@ -38,36 +25,41 @@ const Home=()=>{
         setCurrentPage(pageNumber)
     }
 
-<<<<<<< HEAD
     let dispatch = useDispatch()
     useEffect(()=>{
         dispatch(getGames())
-        dispatch(loadingLoad())  // o seteado en true
+        dispatch(loadingLoad(true))  // o seteado en true
     }, [])
 
+    const handleReaload=(a)=>{
+        a.preventDefault()
+        dispatch(getGames())
+        dispatch(loadingLoad(true))
+    }
 
     return (
         <div>
-
+            <Filter setCurrentPage={setCurrentPage} setOrder={setOrder} />
+            <button onClick={a=>{handleReaload(a)}}>Tezt</button>
         
-
-
+            <div>
+                {currentGame.map((game)=>{
+                    return <Link key={game.id} to={`/games/${game.id}`}>
+                        <Card name={game.name}
+                            image={game.background_image}
+                            genres={(game.id.length > 7) ? game.genres.map(a=>a.name) : game.genres}/>
+                    </Link>
+                })}
+            </div>
+            
+            <div>
+                <Pagination gamesXPage={gamesXPage}
+                            games={games.length}
+                            pagination={pagination}
+                            currentPage={currentPage}/>
+            </div>
 
         </div>
     )
 }
-=======
-    
-    return (
-        <div>
-            <Link to='/'><a>volver</a></Link>
-            <button onClick={e=> {handleClick(e)}}>
-                sarasa
-            </button>
-            <h1>PARATUSALSA</h1>
-        </div>
-    )
-
-}
-export default Home;
->>>>>>> 5bb1eac219e4adf986593038384435e04e67fe19
+export default Videogames
