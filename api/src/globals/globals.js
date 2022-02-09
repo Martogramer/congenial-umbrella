46ba0dff1e78cd0e59e7de43de9114e70c4554bc
Marto <contact@torox.com>
2022-02-08 21:47:32 -0300

    constante globales

--- api/src/globals/globals.js
+++ api/src/globals/globals.js
@@ -1,6 +1,8 @@
 const URL_GAMES = `https://api.rawg.io/api/games?key=6d62af1479864f0cae7616fd7e10a7d2`
 
+const URL_SEARCH = `https://api.rawg.io/api/games?key=6d62af1479864f0cae7616fd7e10a7d2&search=`
 
 module.exports = {
-    URL_GAMES
+    URL_GAMES,
+    URL_SEARCH
 }
