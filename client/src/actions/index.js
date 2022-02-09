5f5fda85058c666aeb918e17d995196121ebe40b
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-02-09 09:36:19 -0300

    actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+export const GET_GAMES = 'GET_GAMES'
+export const LOAD_GAMES = 'LOAD_GAMES'
+
+
 export function getGames(){
     return async function(dispatch) {
         var json = await axios.get("http://localhost:3001/games")
