eb3495d070a6ffe7d2bf0e21cd667aea25ffccde
Marto <contact@torox.com>
2022-02-10 22:30:43 -0300

    ruta principal

--- client/src/index.js
+++ client/src/index.js
@@ -2,24 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
-import {store} from './store/index';
+import generateStore from './store/index';
 import { BrowserRouter } from 'react-router-dom';
 
+const store = generateStore();
 
 ReactDOM.render(
-  <Provider store={store}>
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
+    
       <App />
-    </BrowserRouter>
+    
+  </Provider>
   </React.StrictMode>,
-  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
