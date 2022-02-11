ca9bcdba0d01850f63948ee7a32a5ba8e627c01d
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-02-11 09:11:19 -0300

    actualizacion de importaciones

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
-import generateStore from './store/index';
+import generateStore from './redux/store/index';
 import { BrowserRouter } from 'react-router-dom';
 
 const store = generateStore();
