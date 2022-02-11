f30dc01acbae03fe0f09a9073a834bbcf0a93b2f
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-02-11 17:55:19 -0300

    componente games

--- client/src/components/videogames/Videogames.jsx
+++ client/src/components/videogames/Videogames.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {Link} from 'react-router-dom'
+import styles from './Games.module.css'
 
 import { getGames, loadingLoad } from '../../redux/actions/index';
 import Filter from '../filter/Filter';
@@ -11,14 +12,29 @@ function Videogames(){
 
     const dispatch = useDispatch()
     const games = useSelector(store => store.games)
-    console.log(games)
+    
 
     return (
-        <div>
+        <div className={styles.container}>
 
         asdasf
         <button onClick={() => dispatch(getGames())}>games</button>
-        
+
+        <div className={styles.card}>
+        {
+            games.map(game=>(
+                <Card key={game.id}
+                        name={game.name}
+                        image={game.background_image}
+                        genres={game.genres.map(g=>g.name)}
+                        />
+                ))
+            }    
+        </div>
+
+            
+
+
         </div>
     )
 }
