28a86ca041740ca5c4b3dce0b4e96d46090efb2e
Marto <contact@torox.com>
2022-02-10 22:36:19 -0300

    componente principal videogames

---
+++ client/src/components/videogames/Videogames.jsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Link} from 'react-router-dom'
+
+import { getGames, loadingLoad } from '../../actions';
+import Filter from '../filter/Filter';
+import Card from '../card/Card';
+import Pagination from '../pagination/Pagination';
+
+function Videogames(){
+
+    const dispatch = useDispatch()
+    const games = useSelector(store => store.games)
+    console.log(games)
+
+    return (
+        <div>
+
+        asdasf
+        <button onClick={() => dispatch(getGames())}>games</button>
+        
+
+
+        </div>
+    )
+}
+export default Videogames;
