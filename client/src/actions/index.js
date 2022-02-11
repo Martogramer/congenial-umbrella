eee87277d256262f2727e4c109e25d63e662a6c5
Marto <contact@torox.com>
2022-02-10 22:06:59 -0300

    action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,19 +4,20 @@ export const GET_GAMES = 'GET_GAMES'
 export const LOAD_GAMES = 'LOAD_GAMES'
 
 
-export function getGames(){
-    return async function(dispatch) {
-        var json = await axios.get("http://localhost:3001/games")
-        return dispatch({
-            type: 'GET_GAMES',
-            payload: json.data
+export const getGames=()=>async(dispatch)=>{
+    try {
+        const res = await fetch('http://localhost:3001/games')
+        const data = res.json()
+        dispatch({
+            type: GET_GAMES,
+            payload: data
         })
-    }
+    } catch (err){return err}
 }
 
 export function loadingLoad(payload){
     return{
-        type: 'LOAD_GAMES',
+        type: LOAD_GAMES,
         payload
     }
 }
