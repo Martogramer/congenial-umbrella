49d7ff787b4c3ec5ee8752f7fde216bdb4c0d721
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-02-14 12:06:32 -0300

    action redux ampliado

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const GET_GAMES = 'GET_GAMES'
 export const LOAD_GAMES = 'LOAD_GAMES'
+export const SEARCH_GAMES = 'SEARCH_GAMES'
 
 
 export const getGames=()=>async(dispatch)=>{
@@ -18,5 +19,17 @@ export function loadingLoad(payload){
     return{
         type: LOAD_GAMES,
         payload
+    }
+}
+
+export function searchByName(payload){
+    return async (dispatch)=>{
+        try{
+            const res = await axios.get(`http://localhost:3001/games?name=${payload}`)
+            dispatch({
+                type: SEARCH_GAMES,
+                payload: res.data
+            })
+        }catch(err){return err}
     }
 }
