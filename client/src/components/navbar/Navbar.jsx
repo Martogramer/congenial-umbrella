b9e44513bb1e334e24c8dc91f8824577bff81491
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-02-15 19:19:28 -0300

    new component navbar modified

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Search from "../search/Search";
+import {useDispatch, useSelector} from 'react-redux';
+import { getGames } from '../../redux/actions/index';
+
 
 const Navbar=()=>{
+    const dispatch = useDispatch()
+    const games = useSelector(store => store.games)
+
     return(
         <div>
             <div>
                 <div>
-                    <Link to={'/games'}>Games</Link>
+                    <Link to={'/games'}><button onClick={() => dispatch(getGames())}>games</button></Link>
                 </div>
                 <div>
                     <Search />
