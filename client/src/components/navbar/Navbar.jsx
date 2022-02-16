b18f35e3f0128d6bc5b6856c0fce87d4b9d84a13
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-02-15 22:16:57 -0300

    navbar comp modified

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Search from "../search/Search";
 import {useDispatch, useSelector} from 'react-redux';
 import { getGames } from '../../redux/actions/index';
+import styles from './Navbar.module.css'
 
 
 const Navbar=()=>{
@@ -10,9 +11,9 @@ const Navbar=()=>{
     const games = useSelector(store => store.games)
 
     return(
-        <div>
-            <div>
-                <div>
+        <div className={styles.navbar}>
+            <div className={styles.navbar_links}>
+                <div className={styles.button}>
                     <Link to={'/games'}><button onClick={() => dispatch(getGames())}>games</button></Link>
                 </div>
                 <div>
