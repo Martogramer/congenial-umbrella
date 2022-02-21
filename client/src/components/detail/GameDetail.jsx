11f0fbe35cd101ff9596fb7088364b5c479b4730
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-02-21 20:10:05 -0300

    detail modified

--- client/src/components/detail/GameDetail.jsx
+++ client/src/components/detail/GameDetail.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {getGamesById, loading} from '../../redux/actions/index'
 import Loading from '../loading/Loading'
 import style from './GameDetail.module.css'
+
+const GameDetail=()=>{
+
+    const dispatch=useDispatch()
+    const loadingLoad=useSelector(state=>state.loading)
+    const {id}=useParams()
+
+    useEffect(()=>{
+        dispatch(getGamesById(id))
+        dispatch(loading(true))
+    }, [])
+
+    const gameOne = useSelector(state=>state.game)
+
+return(
+    <div>
+        {!loadingLoad ?
+            <div className={style.container}>
+                <img src={gameOne.background_image} alt=""/>
+                <div className={style.content}>
+                <h1>{gameOne.name}</h1>
+                <p>{gameOne.description}</p>
+
+                </div>
+            </div>
+            : <Loading />
+        }
+    </div>
+)
+}
+export default GameDetail
